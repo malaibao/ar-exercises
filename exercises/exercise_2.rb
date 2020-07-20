e5ae601e7f01ae403e38848192f55f076163b2b4
Maribelle Leong <maribelle.leong@gmail.com>
2020-07-20 16:31:07 -0300

    updated first store name to Coquitlam

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -3,4 +3,7 @@ require_relative './exercise_1'
 
 puts "Exercise 2"
 puts "----------"
-@store1 = Store.find(id: 1)
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+@store1.update(name: 'Coquitlam')
