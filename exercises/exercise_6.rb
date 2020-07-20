c6007a880f1e92b5d0f02314962bdc875d192b97
Maribelle Leong <maribelle.leong@gmail.com>
2020-07-20 20:33:08 -0300

    seeds employees table

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,17 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Maribelle", last_name: "Leong", hourly_rate: 100)
+@store1.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 30)
+@store1.employees.create(first_name: "Martin", last_name: "Garrix", hourly_rate: 50)
+@store1.employees.create(first_name: "Post", last_name: "Malone", hourly_rate: 70)
+
+@store2.employees.create(first_name: "Aaron", last_name: "Smith", hourly_rate: 45)
+@store2.employees.create(first_name: "Sophie", last_name: "Scotts", hourly_rate: 65)
+@store2.employees.create(first_name: "Carmen", last_name: "Wong", hourly_rate: 30)
+
+@store3.employees.create(first_name: "Jaden", last_name: "Simons", hourly_rate: 25)
+@store3.employees.create(first_name: "Yvonne", last_name: "Mantle", hourly_rate: 45)
+@store3.employees.create(first_name: "Sean", last_name: "Lim", hourly_rate: 50)
+@store3.employees.create(first_name: "Steven", last_name: "Cruz", hourly_rate: 45)
