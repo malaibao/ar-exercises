78fbed53a01a6c32b078b01fd24004845c93c7da
Maribelle Leong <maribelle.leong@gmail.com>
2020-07-20 16:53:05 -0300

    loading a subset of stories

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,18 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+Store.create(name: 'Surrey',annual_revenue: 324000, mens_apparel: false, womens_apparel: true)
+Store.create(name: 'Whistler',annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+Store.create(name: 'Yaletown',annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+puts "Stores that sell mens apparel"
+@mens_stores.each do |store|
+    puts "Name: #{store.name} -- Annual revenue: #{store.annual_revenue}"
+end
+
+@womens_lessThan1M_stores = Store.where("womens_apparel = ? AND annual_revenue < ?", true, 1000000)
+puts "Stores that sell women's apparel and generate less than a million annual revenue"
+@womens_lessThan1M_stores.each do |store|
+    puts "Name: #{store.name} -- Annual revenue: #{store.annual_revenue}"
+end
