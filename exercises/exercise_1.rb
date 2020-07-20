305328384800b0c504052f94cd777ce90e022abd
Maribelle Leong <maribelle.leong@gmail.com>
2020-07-20 16:34:28 -0300

    print total of stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -8,3 +8,5 @@ Store.create(name: 'Burnaby',annual_revenue: 300000, mens_apparel: true, womens_
 Store.create(name: 'Richmond',annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 
 Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts "Ex.1 count: #{Store.count}"
