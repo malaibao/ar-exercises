e51fc997b09cd4d7a114e8c5f30af06d9f4a67b7
Maribelle Leong <maribelle.leong@gmail.com>
2020-07-21 11:57:53 -0300

    tested with false in both apparels

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -16,3 +16,11 @@ storeName = gets.chomp
 @createStore = Store.create(name: storeName)
 
 puts @createStore.errors.full_messages
+
+# same thing
+# @createStore.errors.full_messages.each do |err_msg|
+#     puts err_msg
+# end
+
+@createStore2 = Store.create(name: 'Victoria', annual_revenue: 300000, mens_apparel: false, womens_apparel: false)
+puts @createStore2.errors.full_messages
